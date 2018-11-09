5c0f53a1c497e4335921e76e82e10add95560783
Brian Samir Santamaria Valero <alu0101024103@ull.edu.es>
2018-11-09 09:57:43 +0000

    pruebas para las porciones

--- lib/nutricion/version.rb
+++ lib/nutricion/version.rb
@@ -62,36 +62,71 @@ end
     end
 
     def azucar_ir()
-      a_ir= @azucar / 90
+      a_ir= (@azucar / 90)*100
       end
 
     def proteinas_ir()
-       pr_ir= @proteinas/50
+       pr_ir=(@proteinas/50)*100
        end
 
     def sal_ir()
-      s_ir= @sal/6
+      s_ir= (@sal/6)*100
       end
 
     def hidratos_ir()
-      h_ir= @hidratos/260
+      h_ir= (@hidratos/260)*100
       end
 
       def ener_kcal_ir()
-        val_kcal_ir=@@val_kcal/2000
+        val_kcal_ir=(@@val_kcal/2000.00)*100
         end
 
-      def ener_kj_ir()
-       val_kj_ir= @@val_kj/8400
+       def ener_kj_ir()
+       val_kj_ir=(@@val_kj/8400.00)*100
        end
 
-       def grasas_ir()
-        grasita_ir=@grasas/70
+        def grasas_ir()
+        grasita_ir=(@grasas/70)*100
         end
 
       def grasas_sat_ir()
-      gras_sat_ir=@grasas_saturadas/20
+      gras_sat_ir=(@grasas_saturadas/20)*100
+      end
+
+      def get_grasa_por(x)
+        (@grasas * x)/100
       end
 
 
-  end
+      def formato_etiqueta()
+      puts "---------------------------------------------------------------------------------------------------------"
+      puts "                                       Etiqueta"
+      puts get_nombre
+      puts "---------------------------------------------------------------------------------------------------------"
+      puts "                  |   Por 100 g |      IR       |     Por porcion de x g   |       IR por porcion       |"
+      puts "---------------------------------------------------------------------------------------------------------"
+      puts
+      puts " Valor energetico |              |              |                           |                           |"
+    printf "%25d,%d %8d,%6d\n" ,get_val_en_kcal,get_val_en_kj,ener_kcal_ir,ener_kj_ir
+      puts "---------------------------------------------------------------------------------------------------------"
+      puts "  Grasa           |              |              |                           |                           |"
+      printf "%25d%20f \n" ,get_grasa,grasas_ir
+      puts "---------------------------------------------------------------------------------------------------------"
+      puts " Grasas saturadas |              |              |                           |                           |"
+      printf "%25d%20f \n" ,get_grasa_sat,grasas_sat_ir
+      puts "---------------------------------------------------------------------------------------------------------"
+      puts " Hidatos          |              |              |                           |                           |"
+      printf "%25d%20f \n" ,get_hidratos,hidratos_ir
+      puts "---------------------------------------------------------------------------------------------------------"
+      puts " Azúcares          |              |              |                           |                           |"
+      printf "%25d%20f \n" ,get_azucar,azucar_ir
+      puts "---------------------------------------------------------------------------------------------------------"
+      puts " Proteinas         |              |              |                           |                           |"
+      printf "%25d%20f \n" ,get_proteinas,proteinas_ir
+      puts "---------------------------------------------------------------------------------------------------------"
+      puts " Sal               |              |              |                           |                           |"
+    printf "%25d%20f \n" ,get_sal,sal_ir
+      puts "---------------------------------------------------------------------------------------------------------"
+      end
+
+end
--- spec/nutricion_spec.rb
+++ spec/nutricion_spec.rb
@@ -8,7 +8,7 @@ end
 
   describe Etiqueta do
     before :each do
-    @et1=Etiqueta.new('arroz',45.00, 50.00,200.00,150.00,300.00,30.00)
+    @et1=Etiqueta.new('arroz',30.00, 10.00,200.00,70.00,30.00,4.00)
   end
 
     it " Se almacena correctamente el nombre " do
@@ -17,67 +17,103 @@ end
 
 
       it " Se almacena correctamente las grasas " do
-       expect(@et1.get_grasa).to eq(45)
+       expect(@et1.get_grasa).to eq(30.00)
       end
 
       it " Se almacena correctamente las grasas  saturadas" do
-       expect(@et1.get_grasa_sat).to eq(50)
+       expect(@et1.get_grasa_sat).to eq(10.00)
       end
 
       it " Se almacena correctamente los hidratos de carbono" do
-       expect(@et1.get_hidratos).to eq(200)
+       expect(@et1.get_hidratos).to eq(200.00)
       end
 
       it " Se almacena correctamente el azucar" do
-       expect(@et1.get_azucar).to eq(150)
+       expect(@et1.get_azucar).to eq(70.00)
       end
 
       it " Se almacena correctamente las proteinas" do
-       expect(@et1.get_proteinas).to eq(300)
+       expect(@et1.get_proteinas).to eq(30.00)
       end
 
       it " Se almacena correctamente la sal" do
-       expect(@et1.get_sal).to eq(30)
+       expect(@et1.get_sal).to eq(4.00)
       end
 
       it "método de valor energético kcal" do
-        expect(@et1.get_val_en_kcal).to eq(2585)
+        expect(@et1.get_val_en_kcal).to eq(1214.0)
       end
 
       it "método de valor energético kj" do
-        expect(@et1.get_val_en_kj).to eq(8075)
+        expect(@et1.get_val_en_kj).to eq(2488)
       end
 
       it "ingesta de referencia azucar"do
-        expect(@et1.azucar_ir).to eq(1.6666666666666667)
+        expect(@et1.azucar_ir).to eq(77.77777777777779)
       end
 
       it "ingesta de referencia proteinas"do
-      expect(@et1.proteinas_ir).to eq(6)
+        expect(@et1.proteinas_ir).to eq(60.0)
       end
 
       it "ingesta de referencia sal"do
-      expect(@et1.sal_ir).to eq(5)
+        expect(@et1.sal_ir).to eq(66.66666666666666)
       end
 
       it "ingesta de referencia grasas"do
-      expect(@et1.grasas_ir).to eq(0.6428571428571429)
+        expect(@et1.grasas_ir).to eq(42.857142857142854)
       end
 
       it "ingesta de referencia grasas_sat_ir"do
-      expect(@et1.grasas_sat_ir).to eq(2.5)
+        expect(@et1.grasas_sat_ir).to eq(50.0)
       end
 
       it "ingesta de referencia val_en_kcal"do
-      expect(@et1.ener_kcal_ir).to eq(1.2925)
+        expect(@et1.ener_kcal_ir).to eq(60.699999999999996)
       end
 
       it "ingesta de referencia val_en_kj"do
-      expect(@et1.ener_kj_ir).to eq(0.9613095238095238)
+        expect(@et1.ener_kj_ir).to eq(29.619047619047617)
       end
 
       it "ingesta de referencia hidratos"do
-      expect(@et1.hidratos_ir).to eq( 0.7692307692307693)
+      expect(@et1.hidratos_ir).to eq( 76.92307692307693)
       end
 
+      it "porcion grasa x gramos" do
+        expect(@et1.get_grasa_por(30)).to eq(9)
+      end
+
+      it "porcion hidratos x gramos" do
+        expect(@et1.get_hidratos_por(30)).to eq(60)
+      end
+
+      it "porcion sal x gramos" do
+        expect(@et1.get_sal_por(30)).to eq(1.2)
+      end
+
+      it "porcion proteinas x gramos" do
+        expect(@et1.get_proteinas_por(30)).to eq(9)
+      end
+
+      it "porcion val_energetico_kcal  gramos" do
+        expect(@et1.get_val_en_kcal(30)).to eq(364.2)
+      end
+
+      it "porcion azucar x gramos" do
+        expect(@et1.get_azucar_por(30)).to eq(21)
+      end
+
+      it "porcion grasas sat x gramos" do
+        expect(@et1.get_grasa_sat_por(30)).to eq(3)
+      end
+
+      it "porcion val_energetico_kj x gramos" do
+        expect(@et1.get_val_en_kj(30)).to eq(746.4)
+      end
+
+
+
   end
+
+  Etiqueta.new('arroz',30.00, 10.00,200.00,70.00,30.00,4.00).formato_etiqueta
