9688c2427cebb5eb2a6f6a2101203e35beee5658
Brian Samir Santamaria Valero <alu0101024103@ull.edu.es>
2018-11-16 12:10:33 +0000

    codigo para insertar

--- lib/nutricion/lista.rb
+++ lib/nutricion/lista.rb
@@ -7,5 +7,35 @@ class Listas
       @size=0
   end
 
-  
+  def insert(other)
+    if @size==0
+      node_ins=Nodo.new(other,nil,nil)
+      @head= node_ins
+      @tail=@head
+      else
+        node_ins=Nodo.new(other,nil,nil)
+        node_mas=@head
+        node_menos=nil
+        while node_mas != nil do
+
+          if node_ins[:value].get_sal()>=node_mas[:value].get_sal()
+          node_menos=node_mas
+          node_mas=node_mas[:get_next]
+          end
+        end
+
+        node_ins[:get_prev]=node_menos
+        node_ins[:get_next]=node_mas
+
+      if node_menos != nil
+        node_menos[:get_next]=node_ins
+      end
+      if node_mas != nil
+        node_mas[:get_prev]=node_ins
+      end
+
+      end
+      @size+=1;
+    node_ins[:value]
+    end
 end
