99fcbcc11ede9ae7afc46e467a2d68ffd5a64648
Brian Samir Santamaria Valero <alu0101024103@ull.edu.es>
2018-11-23 10:21:15 +0000

    Codigo para los datos de los pacientes(nombre,imc,etc)

--- lib/nutricion/herencia.rb
+++ lib/nutricion/herencia.rb
@@ -1,5 +1,13 @@
 
 class Person
+  attr_reader :nombre,:apellidos,:dni
+
+  def initialize(nombre,apellidos,dni)
+    @nombre,@apellidos,@dni=nombre,apellidos,dni
+  end
+  def to_s()
+     @nombre
+   end
 
 
 
@@ -10,12 +18,47 @@ end
 
 class Paciente<Person
 
+  attr_reader :horario,:telefono,:consulta
+
+  def initialize(nombre,apellidos,dni,horario,telefono,consulta)
+     @horario,@telefono,@consulta=horario,telefono,consulta
+     super(nombre,apellidos,dni)
+  end
 
 end
 
 
 
 class Sobrepeso<Paciente
+  attr_reader :talla,:peso,:edad,:sexo
+    def initialize(talla,peso,edad,sexo,cintura,cadera,array,nombre,apellidos,dni,horario,telefono,consulta)
+      @talla,@peso,@edad,@sexo,@cintura,@cadera,@array=talla,peso,edad,sexo,cintura,cadera,array
+      super(nombre,apellidos,dni,horario,telefono,consulta)
+
+    end
+
+    def imc()
+      @masa = (@peso/(@talla * @talla))
+    end
+
+    def grasa()
+      @grasita= 1.2 * @masa + 0.23 * @edad - 10.8 * @sexo -5.8
+    end
+
+    def rcc()
+      @val_rcc=@cintura/@cadera
+
+    end
+
+    def plieges()
+      @vector=Array.new(4)
+      @vector[0]=(@array[0][0]+@array[0][1]+@array[0][2])/3
+      @vector[1]=(@array[1][0]+@array[1][1]+@array[1][2])/3
+      @vector[2]=(@array[2][0]+@array[2][1]+@array[2][2])/3
+      @vector[3]=(@array[3][0]+@array[3][1]+@array[3][2])/3
+      return @vector
+
+    end
 
 
 
