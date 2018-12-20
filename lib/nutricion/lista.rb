c688ea753054de9820bc60202990f300d264cf5d
Brian Samir Santamaria Valero <alu0101024103@ull.edu.es>
2018-12-20 22:31:21 +0000

    Codigo  para el for  de una lista

--- lib/nutricion/lista.rb
+++ lib/nutricion/lista.rb
@@ -73,6 +73,19 @@ class Listas
       a
     end
 
+    #algoritmo de seleccion(recorro el vector con x para que me de la posicion
+    # y voy ordenandolo en cada iteración con y, en el que esta con el siguiente)
+        def ord_for_lista
+        @au = self.map { |x| x }
 
+        for x in 0..@au.length-1
+          for y in 0..@au.length-2-x
+            if ( @au[y] > @au[y+1] )
+              @au[y], @au[y+1] = @au[y+1], @au[y]
+            end
+          end
+        end
+        return @au
+      end
 
 end
