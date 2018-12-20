8b58722b9c9979593d0642322f3ebc16b6bddafd
Brian Samir Santamaria Valero <alu0101024103@ull.edu.es>
2018-12-20 22:11:58 +0000

    Codigo  para el for de un array

---
+++ lib/nutricion/vectorcito.rb
@@ -0,0 +1,18 @@
+class Array
+
+  #algoritmo de seleccion(recorro el vector con x para que me de la posicion
+  # y voy ordenandolo en cada iteración con y, en el que esta con el siguiente)
+  def ord_for_array
+
+    @au= self
+
+      for x in 0..self.length-1
+        for y in 0..length-2-x
+          if ( @au[y] > @au[y+1] )
+            @au[y], @au[y+1] = @au[y+1], @au[y]
+          end
+        end
+      end
+      return @au
+  end
+end 
